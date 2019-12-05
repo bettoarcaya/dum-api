86aa2e442381ee38fb55518207a0d54d9d8134ea
Humberto Arcaya <betoarcaya@gmail.com>
2019-12-05 19:04:31 -0400

    agregando dinamica al pin de entrada al circuito

--- index.js
+++ index.js
@@ -4,9 +4,29 @@ const cors = require('cors');
 const board = new five.Board();
 
 board.on('ready', () => {
-    let led = new five.Led(13);
     const app = express();
     app.use(cors());
+
+    let analogInput = new five.Pin({
+        pin: "A0"
+    });
+
+    let vccPin = new five.Pin({
+        pin: 7
+    });
+
+    vccPin.high();
+
+    analogInput.read(function(error, value){
+        if(value == 1023){
+            vccPin.low();
+        }
+        if( value == 0 ){
+            vccPin.high();
+        }
+        console.log(value);
+    });
+
     app.get('/', (req, res) => res.send('Ready!'));
     app.listen(8080, () => console.log('Server on: localhost:8080'));
 });
