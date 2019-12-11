6451b0fb38ee33e29bc3b17d861f23f475ba915e
Rafael Baron <baronrafael@hotmail.com>
2019-12-10 21:23:03 -0400

    hola german te amo

--- packages/dum-webapp/src/app/pages/pages.module.ts
+++ packages/dum-webapp/src/app/pages/pages.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { PagesRoutingModule } from './pages-routing.module';
 
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [DashboardComponent],
   imports: [
     CommonModule,
     PagesRoutingModule
