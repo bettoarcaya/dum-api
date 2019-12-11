9580f1dc788a82e5845bc31db827086c19110174
Rafael Baron <baronrafael@hotmail.com>
2019-12-11 00:43:56 -0400

    hago push y ya

--- packages/dum-webapp/src/app/pages/dashboard/dashboard.component.ts
+++ packages/dum-webapp/src/app/pages/dashboard/dashboard.component.ts
@@ -8,36 +8,10 @@ import * as shape from 'd3-shape';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-  view: any[] = [1600, 1400];
-  theme: string = 'dark';
-  colorScheme: string = 'cool';
-  schemeType: string = 'ordinal';
-  plotData: any;
-  mathText = '3 - 1.5*sin(x) + cos(2*x) - 1.5*abs(cos(x))';
-  mathFunction: (o: any) => any;
-  animations: boolean = true;
-  legendTitle = 'Legend';
-  legendPosition = 'right';
-  gradient: boolean = false;
-  showXAxis = true;
-  showYAxis = true;
-  autoScale: boolean = true;
-  timeline: boolean = false;
-  showGridLine: boolean = true;
-  curves = {
-    Cardinal: shape.curveCardinal,
-  };
-  rangeFillOpacity: number = 0.15;
-  roundDomains: boolean = false;
-  tooltipDisabled: boolean = false;
-  trimXAxisTicks = true;
-  trimYAxisTicks = true;
-  maxXAxisTickLength = 16;
-  maxYAxisTickLength = 16;
 
   constructor(private boardService: BoardService) {
-    this.plotData = this.generatePlotData();
-    this.mathFunction = this.getFunction();
+    // this.plotData = this.generatePlotData();
+    // this.mathFunction = this.getFunction();
    }
 
   ngOnInit() {
@@ -47,41 +21,130 @@ export class DashboardComponent implements OnInit {
       });
   }
 
-  generatePlotData() {
-    if (!this.mathFunction) {
-      return [];
-    }
-    const twoPi = 2 * Math.PI;
-    const length = 25;
-    const series = Array.apply(null, { length }).map((d, i) => {
-      const x = i / (length - 1);
-      const t = x * twoPi;
-      return {
-        name: ~~(x * 360),
-        value: this.mathFunction(t)
-      };
-    });
-
-    return [
+  title = 'Angular Charts';
+  view: any[] = [600, 400];
+  // options for the chart
+  showXAxis = true;
+  showYAxis = true;
+  gradient = false;
+  showLegend = true;
+  showXAxisLabel = true;
+  xAxisLabel = 'Country';
+  showYAxisLabel = true;
+  yAxisLabel = 'Sales';
+  timeline = true;
+  colorScheme = {
+    domain: ['#9370DB', '#87CEFA', '#FA8072', '#FF7F50', '#90EE90', '#9370DB']
+  };
+  //pie
+  showLabels = true;
+  // data goes here
+public single = [
+  {
+    "name": "China",
+    "value": 2243772
+  },
+  {
+    "name": "USA",
+    "value": 1126000
+  },
+  {
+    "name": "Norway",
+    "value": 296215
+  },
+  {
+    "name": "Japan",
+    "value": 257363
+  },
+  {
+    "name": "Germany",
+    "value": 196750
+  },
+  {
+    "name": "France",
+    "value": 204617
+  }
+];
+public multi = [
+  {
+    "name": "China",
+    "series": [
       {
-        name: this.mathText,
-        series
+        "name": "2018",
+        "value": 2243772
+      },
+      {
+        "name": "2017",
+        "value": 1227770
       }
-    ];
+    ]
+  },
+  {
+    "name": "USA",
+    "series": [
+      {
+        "name": "2018",
+        "value": 1126000
+      },
+      {
+        "name": "2017",
+        "value": 764666
+      }
+    ]
+  },
+  {
+    "name": "Norway",
+    "series": [
+      {
+        "name": "2018",
+        "value": 296215
+      },
+      {
+        "name": "2017",
+        "value": 209122
+      }
+    ]
+  },
+  {
+    "name": "Japan",
+    "series": [
+      {
+        "name": "2018",
+        "value": 257363
+      },
+      {
+        "name": "2017",
+        "value": 205350
+      }
+    ]
+  },
+  {
+    "name": "Germany",
+    "series": [
+      {
+        "name": "2018",
+        "value": 196750
+      },
+      {
+        "name": "2017",
+        "value": 129246
+      }
+    ]
+  },
+  {
+    "name": "France",
+    "series": [
+      {
+        "name": "2018",
+        "value": 204617
+      },
+      {
+        "name": "2017",
+        "value": 149797
+      }
+    ]
   }
+];
 
-  getFunction(text = this.mathText) {
-    try {
-      text = `with (Math) { return ${this.mathText} }`;
-      // tslint:disable-next-line:function-constructor
-      const fn = new Function('x', text).bind(Math);
-      return typeof fn(1) === 'number' ? fn : null;
-    } catch (err) {
-      return null;
-    }
-  }
 
-  onLegendLabelClick(entry) {
-    console.log('Legend clicked', entry);
-  }
 }
